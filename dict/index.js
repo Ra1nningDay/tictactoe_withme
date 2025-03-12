b71ac3be7372040b9907d847e46d123550cbb8ea
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-12 16:34:20 +0700

    create print board to display board in terminal

---
+++ dict/index.js
@@ -0,0 +1,24 @@
+"use strict";
+const tictac = () => {
+    const board = [[], [], []];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            board[i].push(" ");
+        }
+    }
+
+    const printBoard = () => {
+        console.log("Board TicTacToe:");
+        board.map((row) => {
+            console.log(row.join(" | "));
+        });
+    };
+
+    // const play = () => {
+    //     board =
+    // };
+
+    printBoard();
+};
+
+tictac();
